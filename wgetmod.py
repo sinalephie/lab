4e9e043fed20871359a503369d709afde315783a
sinalephie <148983382+sinalephie@users.noreply.github.com>
2024-01-18 23:56:25 +0100

    Add files via upload

---
+++ wgetmod.py
@@ -0,0 +1,200 @@
+import sys, shutil, os
+import tempfile
+import math
+
+
+import urllib.request
+import urllib.parse
+
+def filename_from_url(url):
+    """:return: detected filename or None"""
+    fname = os.path.basename(urllib.parse.urlparse(url).path)
+    if len(fname.strip(" \n\t.")) == 0:
+        return None
+    return fname
+
+def filename_from_headers(headers):
+    """Detect filename from Content-Disposition headers if present.
+    http://greenbytes.de/tech/tc2231/
+
+    :param: headers as dict, list or string
+    :return: filename from content-disposition header or None
+    """
+    if type(headers) == str:
+        headers = headers.splitlines()
+    if type(headers) == list:
+        headers = dict([x.split(':', 1) for x in headers])
+    cdisp = headers.get("Content-Disposition")
+    if not cdisp:
+        return None
+    cdtype = cdisp.split(';')
+    if len(cdtype) == 1:
+        return None
+    if cdtype[0].strip().lower() not in ('inline', 'attachment'):
+        return None
+    # several filename params is illegal, but just in case
+    fnames = [x for x in cdtype[1:] if x.strip().startswith('filename=')]
+    if len(fnames) > 1:
+        return None
+    name = fnames[0].split('=')[1].strip(' \t"')
+    name = os.path.basename(name)
+    if not name:
+        return None
+    return name
+
+# --- terminal/console output helpers ---
+
+def get_console_width():
+    """Return width of available window area. Autodetection works for
+       Windows and POSIX platforms. Returns 80 for others
+
+       Code from http://bitbucket.org/techtonik/python-pager
+    """
+
+    if os.name == 'nt':
+        STD_INPUT_HANDLE  = -10
+        STD_OUTPUT_HANDLE = -11
+        STD_ERROR_HANDLE  = -12
+
+        # get console handle
+        from ctypes import windll, Structure, byref
+        try:
+            from ctypes.wintypes import SHORT, WORD, DWORD
+        except ImportError:
+            # workaround for missing types in Python 2.5
+            from ctypes import (
+                c_short as SHORT, c_ushort as WORD, c_ulong as DWORD)
+        console_handle = windll.kernel32.GetStdHandle(STD_OUTPUT_HANDLE)
+
+        # CONSOLE_SCREEN_BUFFER_INFO Structure
+        class COORD(Structure):
+            _fields_ = [("X", SHORT), ("Y", SHORT)]
+
+        class SMALL_RECT(Structure):
+            _fields_ = [("Left", SHORT), ("Top", SHORT),
+                        ("Right", SHORT), ("Bottom", SHORT)]
+
+        class CONSOLE_SCREEN_BUFFER_INFO(Structure):
+            _fields_ = [("dwSize", COORD),
+                        ("dwCursorPosition", COORD),
+                        ("wAttributes", WORD),
+                        ("srWindow", SMALL_RECT),
+                        ("dwMaximumWindowSize", DWORD)]
+
+        sbi = CONSOLE_SCREEN_BUFFER_INFO()
+        ret = windll.kernel32.GetConsoleScreenBufferInfo(console_handle, byref(sbi))
+        if ret == 0:
+            return 0
+        return sbi.srWindow.Right+1
+
+    elif os.name == 'posix':
+        from fcntl import ioctl
+        from termios import TIOCGWINSZ
+        from array import array
+
+        winsize = array("H", [0] * 4)
+        try:
+            ioctl(sys.stdout.fileno(), TIOCGWINSZ, winsize)
+        except IOError:
+            pass
+        return (winsize[1], winsize[0])[0]
+
+    return 80
+
+
+def bar_thermometer(current, total, width=80):
+    """Return progress bar string. `total` argument
+    can not be zero. The minimum size of bar returned is 3. Example:
+
+        [########            ]
+
+    Control and trailing symbols (\r and spaces) are not included.
+    See `bar_adaptive` for more information.
+    """
+    # number of dots on thermometer scale
+    avail_dots = width-2
+    shaded_dots = int(math.floor(float(current) / total * avail_dots))
+    return '[' + '#'*shaded_dots + ' '*(avail_dots-shaded_dots) + ']'
+
+def bar_adaptive(current, total, width=80):
+    """
+    """
+
+
+
+# --/ console helpers
+
+
+__current_size = 0  # global state variable, which exists solely as a
+                    # workaround against Python 3.3.0 regression
+                    # http://bugs.python.org/issue16409
+                    # fixed in Python 3.3.1
+def callback_progress(blocks, block_size, total_size, bar_function):
+    """callback function for urlretrieve that is called when connection is
+    created and when once for each block
+
+    draws adaptive progress bar in terminal/console
+
+    use sys.stdout.write() instead of "print,", because it allows one more
+    symbol at the line end without linefeed on Windows
+
+    :param blocks: number of blocks transferred so far
+    :param block_size: in bytes
+    :param total_size: in bytes, can be -1 if server doesn't return it
+    :param bar_function: another callback function to visualize progress
+    """
+    global __current_size
+
+    width = min(100, get_console_width())
+
+    if sys.version_info[:3] == (3, 3, 0):  # regression workaround
+        if blocks == 0:  # first call
+            __current_size = 0
+        else:
+            __current_size += block_size
+        current_size = __current_size
+    else:
+        current_size = min(blocks*block_size, total_size)
+    progress = bar_function(current_size, total_size, width)
+    if progress:
+        sys.stdout.write("\r" + progress)
+
+class ThrowOnErrorOpener(urllib.request.FancyURLopener):
+    def http_error_default(self, url, fp, errcode, errmsg, headers):
+        raise Exception("{0}: {1}".format(errcode, errmsg))
+
+def download(url, out=None, bar=bar_adaptive):
+    names = dict()
+    names["out"] = out or ''
+    names["url"] = filename_from_url(url)
+    # get filename for temp file in current directory
+    prefix = (names["url"] or names["out"] or ".") + "."
+    (fd, tmpfile) = tempfile.mkstemp(".tmp", prefix=prefix, dir=".")
+    os.close(fd)
+    os.unlink(tmpfile)
+
+    # set progress monitoring callback
+    def callback_charged(blocks, block_size, total_size):
+        # 'closure' to set bar drawing function in callback
+        callback_progress(blocks, block_size, total_size, bar_function=bar)
+    if bar:
+        callback = callback_charged
+    else:
+        callback = None
+
+    (tmpfile, headers) = ThrowOnErrorOpener().retrieve(url, tmpfile, callback)
+    names["header"] = filename_from_headers(headers)
+    if os.path.isdir(names["out"]):
+        filename = names["header"] or names["url"]
+        filename = names["out"] + "/" + filename
+    else:
+        filename = names["out"] or names["header"] or names["url"]
+    # add numeric ' (x)' suffix if filename already exists
+    if os.path.exists(filename):
+      if os.path.isdir(names["out"]):
+          filename = names["header"] or names["url"]
+          filename = names["out"] + "/" + filename
+      else:
+          filename = names["out"] or names["header"] or names["url"]
+    shutil.move(tmpfile, filename)
+
