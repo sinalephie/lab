aa1431711493f4b97e136ca58272ae5da04b6097
sinalephie <148983382+sinalephie@users.noreply.github.com>
2024-01-19 01:22:24 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='nomedelpacchetto',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # Lista delle dipendenze del tuo pacchetto
+    ],
+    entry_points={
+        'console_scripts': [
+            # Eventuali script eseguibili
+        ],
+    },
+    # Altre informazioni come autore, email, descrizione, ecc.
+)
