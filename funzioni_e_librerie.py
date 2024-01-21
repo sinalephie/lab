f81fab3593f6bb7de672a448cc1823b6f9822252
sinalephie <148983382+sinalephie@users.noreply.github.com>
2024-01-21 15:12:40 +0100

    Update funzioni_e_librerie.py

--- funzioni_e_librerie.py
+++ funzioni_e_librerie.py
@@ -11,7 +11,7 @@ import requests
 from io import BytesIO
 from termcolor import colored
 from lab.librerie_aggiuntive.uncertainties.core import ufloat, correlated_values
-from lab.librerie_aggiuntive.wgetmod as wget
+from lab.librerie_aggiuntive.wgetmod as wgetmod
 #VARIABILI
 rispostacorretta='https://cdn.pixabay.com/download/audio/2021/08/04/audio_bb630cc098.mp3?filename=short-success-sound-glockenspiel-treasure-video-game-6346.mp3'
 
@@ -19,7 +19,7 @@ rispostacorretta='https://cdn.pixabay.com/download/audio/2021/08/04/audio_bb630c
 #FUNZIONI
 def importa(link):
     import lab.wgetmod 
-    wget.download(link)
+    wgetmod.download(link)
     return
 
 
