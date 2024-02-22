6b0c3c7835a9c33dadd859525a53bb62d9707a3c
sinalephie <148983382+sinalephie@users.noreply.github.com>
2024-02-22 18:05:01 +0100

    Update lab.py

--- lab.py
+++ lab.py
@@ -144,7 +144,7 @@ def stampa(*frasi_grandezze_colore):
           color='black'
         else:
           color='white'
-    except ModuleNotFoundError:
+    except ModuleNotFoundError, NameError:
         color='grey'
     testo_stili_multipli = ""
     frase = ''
