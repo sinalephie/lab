0ae1a24b790458151ff48649039709ff4c0193f8
sinalephie <148983382+sinalephie@users.noreply.github.com>
2024-03-03 00:01:44 +0100

    Update lab.py

--- lab.py
+++ lab.py
@@ -52,11 +52,7 @@ suoni['yumi'] = 'https://drive.usercontent.google.com/download?id=1TYoZtfyXbDXGv
 def stile(a):
   c=['default','labs/librerie_aggiuntive/SciencePlots/stile.mplstyle','labs/librerie_aggiuntive/SciencePlots/stile 2.mplstyle','labs/librerie_aggiuntive/SciencePlots/science.mplstyle','Solarize_Light2','_classic_test_patch', '_mpl-gallery', '_mpl-gallery-nogrid','bmh','classic', 'dark_background','fast','fivethirtyeight','ggplot','grayscale','seaborn-v0_8','seaborn-v0_8-bright','seaborn-v0_8-colorblind','seaborn-v0_8-dark', 'seaborn-v0_8-dark-palette','seaborn-v0_8-darkgrid','seaborn-v0_8-deep','seaborn-v0_8-muted','seaborn-v0_8-notebook','seaborn-v0_8-paper','seaborn-v0_8-pastel', 'seaborn-v0_8-poster','seaborn-v0_8-talk','seaborn-v0_8-ticks','seaborn-v0_8-white','seaborn-v0_8-whitegrid','tableau-colorblind10']
   try:
-    if a==0:
-      c='default'
-    if a==1:
-      c='labs/librerie_aggiuntive/SciencePlots/stile.mplstyle'
-    return c
+    return c[a]
   except:
     return 'default'
 def linkdiretto(link):
