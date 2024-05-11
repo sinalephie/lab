5c87bb98f5564f55c5d6d5a7e485547c277a9660
sinalephie <148983382+sinalephie@users.noreply.github.com>
2024-05-11 22:00:42 +0200

    Update lab.py

--- lab.py
+++ lab.py
@@ -77,10 +77,17 @@ def riordina(*args,**kwargs):
 def stile(a):
   plt.style.use('default')
   c=['default','labs/librerie_aggiuntive/SciencePlots/stile.mplstyle','labs/librerie_aggiuntive/SciencePlots/stile 2.mplstyle','labs/librerie_aggiuntive/SciencePlots/science.mplstyle','Solarize_Light2','_classic_test_patch','bmh','classic', 'dark_background','fast','fivethirtyeight','ggplot','grayscale','seaborn-v0_8','seaborn-v0_8-bright','seaborn-v0_8-colorblind','seaborn-v0_8-dark', 'seaborn-v0_8-dark-palette','seaborn-v0_8-darkgrid','seaborn-v0_8-deep','seaborn-v0_8-muted','seaborn-v0_8-notebook','seaborn-v0_8-paper','seaborn-v0_8-pastel', 'seaborn-v0_8-poster','seaborn-v0_8-talk','seaborn-v0_8-ticks','seaborn-v0_8-white','seaborn-v0_8-whitegrid','tableau-colorblind10']
-  try:
-    return plt.style.use(c[a])
-  except:
-    return plt.style.use('default')
+  if isinstance(a,int):
+    try:
+      return plt.style.use(c[a])
+    except:
+      return plt.style.use('default')
+  else:
+    try:
+      ciao='labs/librerie_aggiuntive/SciencePlots/'+{c}
+      return plt.style.use(ciao)
+    except:
+      return plt.style.use('default')
 def linkdiretto(link):
   import os, re, urllib,shutil
   try:
