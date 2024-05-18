2e54f9e47f65d6e1b6124f568902a346b2b30e0a
sinalephie <148983382+sinalephie@users.noreply.github.com>
2024-05-18 23:39:41 +0200

    Update lab.py

--- lab.py
+++ lab.py
@@ -81,6 +81,58 @@ def stile(a,linee=True,scatter=True,riempimento=True,colore_assi='white',colore_
     plt.gcf().set_facecolor(f'{colore_assi}')
   if colore_sfondo != 'white':
     plt.gca().set_facecolor(f'{colore_sfondo}')
+  if a=='CMS' or a=='ROOT':
+    from cycler import cycler
+    cmap_petroff = ["#5790fc", "#f89c20", "#e42536", "#964a8b", "#9c9ca1", "#7a21dd"]
+    CMS = {
+        "axes.prop_cycle": cycler("color", cmap_petroff),
+        "font.sans-serif": ["TeX Gyre Heros", "Helvetica", "Arial"],
+        "font.family": "sans-serif",
+        "mathtext.fontset": "custom",
+        "mathtext.rm": "TeX Gyre Heros",
+        "mathtext.bf": "TeX Gyre Heros:bold",
+        "mathtext.sf": "TeX Gyre Heros",
+        "mathtext.it": "TeX Gyre Heros:italic",
+        "mathtext.tt": "TeX Gyre Heros",
+        "mathtext.cal": "TeX Gyre Heros",
+        "mathtext.default": "regular",
+        "figure.figsize": (10.0, 10.0),
+        "font.size": 26,
+        "axes.labelsize": "medium",
+        "axes.unicode_minus": False,
+        "xtick.labelsize": "small",
+        "ytick.labelsize": "small",
+        "legend.fontsize": "small",
+        "legend.handlelength": 1.5,
+        "legend.borderpad": 0.5,
+        "legend.frameon": False,
+        "xtick.direction": "in",
+        "xtick.major.size": 12,
+        "xtick.minor.size": 6,
+        "xtick.major.pad": 6,
+        "xtick.top": True,
+        "xtick.major.top": True,
+        "xtick.major.bottom": True,
+        "xtick.minor.top": True,
+        "xtick.minor.bottom": True,
+        "xtick.minor.visible": True,
+        "ytick.direction": "in",
+        "ytick.major.size": 12,
+        "ytick.minor.size": 6.0,
+        "ytick.right": True,
+        "ytick.major.left": True,
+        "ytick.major.right": True,
+        "ytick.minor.left": True,
+        "ytick.minor.right": True,
+        "ytick.minor.visible": True,
+        "grid.alpha": 0.8,
+        "grid.linestyle": ":",
+        "axes.linewidth": 2,
+        "savefig.transparent": False,
+        "xaxis.labellocation": "right",
+        "yaxis.labellocation": "top",
+    }
+    plt.rcParams.update(CMS)
   if a=='glow':
     from labs.librerie_aggiuntive.cyberpunk import make_lines_glow, add_underglow, make_scatter_glow
     if scatter==True:
