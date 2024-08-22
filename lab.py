8ce9195cb4bc815997cc802c02d4089207303567
sinalephie <148983382+sinalephie@users.noreply.github.com>
2024-08-22 18:04:30 +0200

    Update lab.py

--- lab.py
+++ lab.py
@@ -140,6 +140,7 @@ def stile(*args,glow_linee=False,glow_punti=False,riempimento=False,colore_assi=
     add_underglow()
   if len(args)!=0:
     plt.style.use('default')
+    plt.style.use('default')
     for a in args: 
       c=['default','labs/librerie_aggiuntive/SciencePlots/stile.mplstyle','labs/librerie_aggiuntive/SciencePlots/stile 2.mplstyle','labs/librerie_aggiuntive/SciencePlots/science.mplstyle','Solarize_Light2','_classic_test_patch','bmh','classic', 'dark_background','fast','fivethirtyeight','ggplot','grayscale','seaborn-v0_8','seaborn-v0_8-bright','seaborn-v0_8-colorblind','seaborn-v0_8-dark', 'seaborn-v0_8-dark-palette','seaborn-v0_8-darkgrid','seaborn-v0_8-deep','seaborn-v0_8-muted','seaborn-v0_8-notebook','seaborn-v0_8-paper','seaborn-v0_8-pastel', 'seaborn-v0_8-poster','seaborn-v0_8-talk','seaborn-v0_8-ticks','seaborn-v0_8-white','seaborn-v0_8-whitegrid','tableau-colorblind10']
       if isinstance(a,int):
